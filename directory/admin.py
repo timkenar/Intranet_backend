386158424960d386302b0137716b3dbc1badfdf9
Timothy Kiprop <chepkenertimothy@gmail.com>
2024-10-27 16:13:32 +0300

    Registered Directory App to admin Site

--- directory/admin.py
+++ directory/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .models import Role, UserRole
+
+from django import forms
+
+class UserRoleForm(forms.ModelForm):
+    class Meta:
+        model = UserRole
+        fields = ['role']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['role'].widget = forms.RadioSelect()
+        self.fields['role'].queryset = Role.objects.all()
+
+class UserRoleInline(admin.TabularInline):
+    model = UserRole
+    form = UserRoleForm
+    extra = 1
+class UserRoleInline(admin.TabularInline):
+    model = UserRole
+    extra = 1
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserRoleInline,)
+
+
+
+admin.site.register(Role)
+
+admin.site.register(UserRole)
