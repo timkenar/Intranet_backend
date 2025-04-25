492ca1b64df9f1ea5a2289d050d225659f4bf242
timkenar <chepkenertimothy@gmail.com>
2025-04-25 20:13:47 +0300

    Removed Visible SMTP

--- intranet_backend/settings.py
+++ intranet_backend/settings.py
@@ -178,14 +178,6 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'kenartutors@gmail.com'
 EMAIL_HOST_PASSWORD = 'scaqkbyqhehzrskr'
 
-# Kmtc SMTP DETAILS
-
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'eboard@kmtc.ac.ke'
-# EMAIL_HOST_PASSWORD = 'livkleivltnmdvnx'
 
 # New configurations email with noreply domain
 # Email configuration using SSL for port 465
